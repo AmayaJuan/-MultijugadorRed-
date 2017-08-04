55f1025f2d1d5012ba7627a8ea9c3922f80ef570
AmayaJuan <jpva360@gmail.com>
2017-08-04 14:41:27 -0500

    Tutorial 14
    Muerte y reaparicion

--- uNet/Assets/MultijugadorRed/Script/Health.cs
+++ uNet/Assets/MultijugadorRed/Script/Health.cs
@@ -1,21 +1,37 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class Health : MonoBehaviour
+public class Health : NetworkBehaviour
 {
     public const int maxHealth = 100;
+    [SyncVar(hook = "OnChangeHealth")]
     public int currentHealth = maxHealth;
     public RectTransform healthBar;
 
     public void TakeDamage(int amount)
     {
+        if (!isServer)
+            return;
+
         currentHealth -= amount;
         if (currentHealth <= 0)
         {
-            currentHealth = 0;
-            Debug.Log("Dead!");
+            currentHealth = maxHealth;
+            RpcRespawn();
         }
+    }
+
+    void OnChangeHealth(int currentHealth)
+    {
         healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+            transform.position = Vector3.zero;
     }
 }
