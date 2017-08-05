919a6275166279050a714951980101d7fba97595
AmayaJuan <jpva360@gmail.com>
2017-08-04 21:39:10 -0500

    Tutorial 19
    Retoques fonales

---
+++ uNet/Assets/MultijugadorRed/Script/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Menu : MonoBehaviour
+{
+    MyNetworkDiscovery myNetworkDiscovery;
+
+    void Awake()
+    {
+        myNetworkDiscovery = FindObjectOfType<MyNetworkDiscovery>();
+    }
+
+    void Start()
+    {
+        StopDiscovery();
+    }
+
+    public void CrearUnaPartida()
+    {
+        StopDiscovery();
+        myNetworkDiscovery.StartAsServer();
+        NetworkManager.singleton.StartHost();
+    }
+
+    public void UnirseAUnaPartida()
+    {
+        StopDiscovery();
+        myNetworkDiscovery.StartAsClient();
+    }
+
+    void StopDiscovery()
+    {
+        if (myNetworkDiscovery.running)
+            myNetworkDiscovery.StopBroadcast();
+    }
+}
